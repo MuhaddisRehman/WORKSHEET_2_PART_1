70fc2d834e3d9ca23710fbc3f39f1153c9a9e75e
MuhaddisRehman <muhaddisrehman12@gmail.com>
2025-12-05 14:38:20 +0500

    comment

--- source/kernel.c
+++ source/kernel.c
@@ -4,22 +4,11 @@
 #include "drivers/interrupts.h"
 
 void kernel_main() {
-    // Initial debug output before enabling interrupts
     fb_clear(FB_LIGHT_GREY, FB_BLACK);
-    // fb_print("Kernel started...\n", FB_LIGHT_GREEN, FB_BLACK);
-
-    // fb_print("Setting up interrupts...\n", FB_LIGHT_CYAN, FB_BLACK);
     interrupts_install_idt();
-    // fb_print("IDT installed!\n", FB_LIGHT_CYAN, FB_BLACK);
-
-    // fb_print("Initializing terminal...\n", FB_LIGHT_CYAN, FB_BLACK);
     terminal_init();
-    // fb_print("Terminal ready!\n", FB_LIGHT_CYAN, FB_BLACK);
-
-    // Enable hardware interrupts **after framebuffer setup**
-    // enable_hardware_interrupts();
-    // fb_print("Interrupts enabled!\n", FB_LIGHT_GREEN, FB_BLACK);
-
+    // enable_hardware_interrupts(); Uncomment this line to enable hardware interrupts
+    
     // Start main terminal loop
     terminal_run();
 }
